f93abef24a184df40e99f3b374efe187118b3d3c
Andreas Borba <borba_andreas@hotmail.com>
2025-01-15 23:13:16 -0300

    Ajuste na validação dos testes unitários - inclusão @Mock DatabaseService

--- src/test/java/com/br/projetoAPI/projetoAPI/unit/CepServiceTest.java
+++ src/test/java/com/br/projetoAPI/projetoAPI/unit/CepServiceTest.java
@@ -5,6 +5,8 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
 import com.br.projetoAPI.projetoAPI.Model.CepResponse;
 import com.br.projetoAPI.projetoAPI.Service.CepService;
+import com.br.projetoAPI.projetoAPI.Service.DatabaseService;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +23,11 @@ public class CepServiceTest {
 
     @InjectMocks
     private CepService cepService;
+    
+    @Mock
+    private DatabaseService databaseService;
 
+    @Test
     public void testConsultarCepValido() {
         String cep = "01001000";
         CepResponse mockResponse = new CepResponse();
