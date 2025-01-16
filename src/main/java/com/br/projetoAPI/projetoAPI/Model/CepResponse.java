0bed0e0a0978409b11f24c745b5bce38046dd1f9
Andreas Borba <borba_andreas@hotmail.com>
2025-01-16 01:13:15 -0300

    Exclusão de pasta e indentar o código

--- src/main/java/com/br/projetoAPI/projetoAPI/Model/CepResponse.java
+++ src/main/java/com/br/projetoAPI/projetoAPI/Model/CepResponse.java
@@ -6,34 +6,43 @@ public class CepResponse {
     private String city;
     private String neighborhood;
     private String street;
-    
+
     public String getCep() {
         return cep;
     }
+
     public void setCep(String cep) {
         this.cep = cep;
     }
+
     public String getState() {
         return state;
     }
+
     public void setState(String state) {
         this.state = state;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public String getNeighborhood() {
         return neighborhood;
     }
+
     public void setNeighborhood(String neighborhood) {
         this.neighborhood = neighborhood;
     }
+
     public String getStreet() {
         return street;
     }
+
     public void setStreet(String street) {
         this.street = street;
     }
